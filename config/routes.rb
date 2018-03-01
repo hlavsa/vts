6486f9d69b047f4dca1c0435ac62a369ee7d7ee0
hlavsa <hlavsa.jara@seznam.cz>
2018-03-01 14:39:24 +0100

    tabs on main page, tables fixed

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get 'claims' => 'pages#claims', as: :claims
   get 'login' => 'pages#login', as: :login
   get 'table' => 'pages#table', as: :table
+  get 'favorites' => 'pages#favorites', as: :favorites
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
